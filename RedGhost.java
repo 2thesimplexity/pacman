8eb76bdb864d6258864f1ca1c32873585ba87b60
Eeshan Mishra <55258933+EeshanMishra@users.noreply.github.com>
2023-07-03 21:27:34 -0700

    As a red ghost, make angry sounds, move, and draw red ghost (3 user stories)

--- RedGhost.java
+++ RedGhost.java
@@ -3,6 +3,36 @@ import java.awt.*;
 public class RedGhost implements Drawable{
     private int positionX = 300;
     private int positionY = 400;
+    private RedSoundPlayer soundPlayer;
+
+    public RedGhost() {
+        soundPlayer = new RedSoundPlayer();
+        soundPlayer.startSoundPlayback();
+    }
+
+    class RedSoundPlayer {
+        private static final String AUDIO_FILE_PATH = "/Users/eeshanmishra/IdeaProjects/Pac-Man/src/10.wav";
+
+        public void startSoundPlayback() {
+            Timer timer = new Timer();
+            timer.schedule(new PlaySoundTask(), 0, 30000); // Schedule the task to run every minute
+        }
+
+        class PlaySoundTask extends TimerTask {
+            @Override
+            public void run() {
+                try {
+                    File audioFile = new File(AUDIO_FILE_PATH);
+                    AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+                    Clip clip = AudioSystem.getClip();
+                    clip.open(audioStream);
+                    clip.start();
+                } catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     public void draw(Graphics g){
         g.setColor(Color.RED);
