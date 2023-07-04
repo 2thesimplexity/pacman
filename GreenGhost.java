32d56bdf7f491938b580f8bd90886e6b5fa49f9a
Eeshan Mishra <55258933+EeshanMishra@users.noreply.github.com>
2023-07-03 21:25:55 -0700

    As a green ghost, make angry sounds, move, and draw green ghost (3 user stories)

--- GreenGhost.java
+++ GreenGhost.java
@@ -3,6 +3,35 @@ import java.awt.*;
 public class GreenGhost implements Drawable{
     private int positionX = 400;
     private int positionY = 80;
+    private GreenSoundPlayer soundPlayer;
+
+    public GreenGhost() {
+        soundPlayer = new GreenSoundPlayer();
+        soundPlayer.startSoundPlayback();
+    }
+    class GreenSoundPlayer {
+        private static final String AUDIO_FILE_PATH = "/Users/eeshanmishra/IdeaProjects/Pac-Man/src/24.wav";
+
+        public void startSoundPlayback() {
+            Timer timer = new Timer();
+            timer.schedule(new GreenSoundPlayer.PlaySoundTask(), 0, 60000); // Schedule the task to run every minute
+        }
+
+        class PlaySoundTask extends TimerTask {
+            @Override
+            public void run() {
+                try {
+                    File audioFile = new File(AUDIO_FILE_PATH);
+                    AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+                    Clip clip = AudioSystem.getClip();
+                    clip.open(audioStream);
+                    clip.start();
+                } catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     public void draw(Graphics g){
         g.setColor(Color.GREEN);
